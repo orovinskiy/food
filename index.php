eb1132f16b03321f17ee22e233aff2192899a40f
Oleg Rovinskiy <orovinskiy@mail.greenriver.edu>
2020-01-22 13:35:22 -0500

    Routes added

--- index.php
+++ index.php
@@ -1,15 +1,27 @@
 <?php
+/*
+ * Oleg Rovinskiy
+ * 1/22/2020
+ * it_328/food
+ */
+
 //Turn on error reporting
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-require("vendor/autoload.php");
+require_once("vendor/autoload.php");
 
 $f3 = Base::instance();
 
-$f3->route("Get /", function(){
+$f3->route("GET /", function(){
     $view = new Template();
-    echo $view->render("views/");
+    echo $view->render("views/home.html");
+});
+
+//Breakfast route
+$f3->route('GET /breakfast', function(){
+    $view = new Template();
+    $view->render('views/breakfast.html');
 });
 
 $f3->run();
